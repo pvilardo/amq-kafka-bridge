de3928168877fc74384bb17a117aa76fb97d3579
pvilardo <pvilardo@primary.com.ar>
2020-06-25 13:34:14 -0300

    add routes for orders & ER

--- src/main/java/ar/com/primary/eventbridge/EventBridgeApplication.java
+++ src/main/java/ar/com/primary/eventbridge/EventBridgeApplication.java
@@ -1,7 +1,14 @@
 package ar.com.primary.eventbridge;
 
+import ar.com.primary.ptp.jms.FSTMessageConverter;
+import ar.com.primary.ptp.model.datacontainers.HasOrderHeader;
+import ar.com.primary.ptp.model.matchingengine.OrderHeader;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.jms.support.converter.MessageConverter;
 
 @SpringBootApplication
 public class EventBridgeApplication {
@@ -9,5 +16,12 @@ public class EventBridgeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EventBridgeApplication.class, args);
 	}
+
+	@Bean(name = "fstConverter")
+	public MessageConverter getMessageConverter(){
+		return new FSTMessageConverter();
+	}
+
+
 
 }
